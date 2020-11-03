ab8825798ace7018318cb33d44c74e4377498161
seochoa <seochoa@unicesar.edu.co>
2020-11-03 08:56:21 -0500

    3cercommit

--- Parcial/ClientApp/src/app/app.module.ts
+++ Parcial/ClientApp/src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormBuilder, FormsModule } from '@angular/forms';
+import { FormBuilder, FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 import { RouterModule } from '@angular/router';
 
@@ -25,7 +25,6 @@ import { FiltroPersonaPipe } from './pipe/filtro-persona.pipe';
     FiltroPersonaPipe
   ],
   imports: [
-    FormBuilder,
     BrowserModule.withServerTransition({ appId: 'ng-cli-universal' }),
     HttpClientModule,
     FormsModule,
